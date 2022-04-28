f3b1895ae13662f740a98a5f9825076f8393e8f8
Mishaaaaaaa <94446962+MishaWhitehat@users.noreply.github.com>
2022-04-28 16:41:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+menu_list_array = ["Veg Margherita Pizza",
+                        "Chicken Mushrrom pizza",
+                        "Kimchi ramen",
+                        "Paneer Tikka Pizza",
+                        "Deluxe Veggie Pizza",
+                        "Cheesy garlic bread",
+                        "Pene Pasta with veggies",
+                        "Pene Pasta with veggies and chicken",
+                        "Veg Extravaganza Pizza"];
+    
+    function getmenu(){
+        var htmldata="";
+        menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+    }
+
+    function add_item(){
+        var htmldata;
+		var imgtags='<img id="im1" src="images/RED.png"/>'
+        var item=document.getElementById("add_item").value;
+        menu_list_array.sort();
+        htmldata=""
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;
+		
+    }
+
+function add_top(){
+	var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
